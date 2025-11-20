3dc39a28f216e0380ff24c02cbd47fcd3493404a
OptiTradeAi <diegobinox88@gmail.com>
2025-11-20 11:55:23 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,19 +1,6 @@
-import base64
-import cv2
-import numpy as np
-from PIL import Image
-import io
+import os
 
-
-def screenshot_to_webp(driver, target_width=350):
-    png = driver.get_screenshot_as_png()
-    img = Image.open(io.BytesIO(png))
-
-    w, h = img.size
-    ratio = target_width / float(w)
-    new_size = (target_width, int(h * ratio))
-    img = img.resize(new_size)
-
-    buffer = io.BytesIO()
-    img.save(buffer, format="WEBP", quality=70)
-    return base64.b64encode(buffer.getvalue()).decode()
+def save_screenshot(driver, filename):
+    path = os.path.join("/app", filename)
+    driver.save_screenshot(path)
+    print(f"[Screenshot] Salvo em {path}")
