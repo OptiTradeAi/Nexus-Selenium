c6b4c41fb571540c7ce6f2d1c09d90253a96f967
OptiTradeAi <diegobinox88@gmail.com>
2025-11-20 19:16:03 -0300

    Update selenium_core.py

--- selenium_core.py
+++ selenium_core.py
@@ -1,26 +1,12 @@
-from selenium import webdriver
-from selenium_stealth import stealth
+import undetected_chromedriver as uc
 
 def create_driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--disable-blink-features=AutomationControlled")
+    options = uc.ChromeOptions()
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--start-maximized")
     options.add_argument("--window-size=1920,1080")
+    # Descomente a linha abaixo para rodar em modo headless, se desejar
+    # options.add_argument("--headless=new")
 
-    # Headless novo do Chrome
-    options.add_argument("--headless=new")
-
-    driver = webdriver.Chrome(options=options)
-
-    stealth(driver,
-            languages=["pt-BR", "pt"],
-            vendor="Google Inc.",
-            platform="Win32",
-            webgl_vendor="Intel Inc.",
-            renderer="Intel Iris OpenGL Engine",
-            fix_hairline=True,
-            )
-
+    driver = uc.Chrome(options=options)
     return driver
