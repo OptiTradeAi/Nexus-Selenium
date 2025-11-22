279ea94fbc04c786264a5a721bac532d971c228b
OptiTradeAi <diegobinox88@gmail.com>
2025-11-22 16:21:27 -0300

    Update selenium_core.py

--- selenium_core.py
+++ selenium_core.py
@@ -1,67 +1,61 @@
-import os
-import json
 import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+import json
+import os
+import undetected_chromedriver as uc
 
-OUTPUT_PATH = "/app/data/learned_selectors.json"
+HOME_BROKER_URL = "https://www.homebroker.com/pt/invest"
 
-HOME_URL = "https://www.homebroker.com/pt/sign-in"
+class NexusSelenium:
+    def __init__(self):
+        self.driver = None
 
+    def start(self):
+        print("[nexus] iniciando chrome…")
+        options = uc.ChromeOptions()
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--window-size=1280,720")
 
-def try_select(driver, selectors):
-    for s in selectors:
-        try:
-            element = driver.find_element(By.CSS_SELECTOR, s)
-            return s
-        except:
-            pass
-    return None
+        self.driver = uc.Chrome(options=options)
 
+        print("[nexus] abrindo corretora…")
+        self.driver.get(HOME_BROKER_URL)
+        time.sleep(8)
 
-def perform_dom_learning():
-    print("[selenium_core] Starting DOM learning session...")
+        print("[nexus] iniciando varredura…")
+        scan_data = self.scan_page()
 
-    chrome_options = Options()
-    chrome_options.add_argument("--headless=new")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+        print("[nexus] salvando dados…")
+        self.save_scan(scan_data)
 
-    driver = webdriver.Chrome(options=chrome_options)
+    def scan_page(self):
+        data = {
+            "url": self.driver.current_url,
+            "title": self.driver.title,
+            "elements": []
+        }
 
-    driver.get(HOME_URL)
-    time.sleep(5)
+        elems = self.driver.find_elements("xpath", "//*")
+        for e in elems:
+            try:
+                info = {
+                    "tag": e.tag_name,
+                    "type": e.get_attribute("type"),
+                    "id": e.get_attribute("id"),
+                    "name": e.get_attribute("name"),
+                    "class": e.get_attribute("class"),
+                    "text": e.text[:200]
+                }
+                data["elements"].append(info)
+            except:
+                pass
 
-    # tentativas de seletores
-    email_selectors = [
-        "input[type=email]",
-        "input[name=email]",
-        "input[placeholder*=mail]",
-        "input[id*=email]",
-        "input"
-    ]
+        return data
 
-    password_selectors = [
-        "input[type=password]",
-        "input[name=password]",
-        "input[placeholder*=senha]",
-        "input[id*=password]",
-        "input"
-    ]
-
-    found_email = try_select(driver, email_selectors)
-    found_password = try_select(driver, password_selectors)
-
-    result = {
-        "email_selector": found_email,
-        "password_selector": found_password
-    }
-
-    with open(OUTPUT_PATH, "w") as f:
-        json.dump(result, f, indent=4)
-
-    driver.quit()
-
-    print("[selenium_core] DOM learning completed.", result)
-    return result
+    def save_scan(self, data):
+        os.makedirs("/app/data", exist_ok=True)
+        with open("/app/data/scan.json", "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+        print("[nexus] scan salvo em /app/data/scan.json")
