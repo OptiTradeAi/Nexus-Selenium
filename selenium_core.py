ac5c95bbd47e162babb867b6a84d69ec3ea5ca04
OptiTradeAi <diegobinox88@gmail.com>
2025-11-24 14:53:17 -0300

    Update selenium_core.py

--- selenium_core.py
+++ selenium_core.py
@@ -2,7 +2,6 @@ import time
 import threading
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 
 class SeleniumCore:
     def __init__(self):
@@ -14,11 +13,14 @@ class SeleniumCore:
 
         chrome_options = Options()
         chrome_options.add_argument("--headless=new")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+        chrome_options.add_argument("--remote-debugging-port=9222")
         chrome_options.add_argument("--window-size=1920,1080")
+
+        chrome_options.binary_location = "/usr/bin/google-chrome"
 
         try:
             self.driver = webdriver.Chrome(
@@ -31,13 +33,14 @@ class SeleniumCore:
             print("[selenium_core] falha criando driver:", e)
             return
 
+        # Manter vivo
         threading.Thread(target=self._keep_alive, daemon=True).start()
 
     def _keep_alive(self):
         while True:
             try:
-                self.driver.title
-            except:
+                _ = self.driver.title
+            except Exception:
                 print("[selenium_core] driver desconectado")
                 break
             time.sleep(5)
