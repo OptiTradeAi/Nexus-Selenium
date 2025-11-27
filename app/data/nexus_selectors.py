51cc9863c8c9656da1eb5082115a79e21e1eb3a4
OptiTradeAi <diegobinox88@gmail.com>
2025-11-27 11:35:27 -0300

    Create nexus_selectors.py

---
+++ app/data/nexus_selectors.py
@@ -0,0 +1,5 @@
+{
+  "email": "/html/body/div[2]/div/div/div/form/div/div/input",
+  "password": "/html/body/div[2]/div/div/div/form/div/div[2]/div/input",
+  "submit": "//button[@type='submit']"
+}
